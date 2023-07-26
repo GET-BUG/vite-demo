2ce720c31a480f49093b8835d7a6b911d73aca41
liji <ji.li@acmeict.com>
2023-07-26 18:20:52 +0800

    12

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,13 @@ import store from './store/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import VideoPlayer from 'vue-video-player'
+import 'video.js/dist/video-js.css'
+// import 'vue-video-player/src/custom-theme.css'
 
 
 
 
 const app = createApp(App);
-app.use(router).use(store).use(ElementPlus);
+app.use(router).use(store).use(ElementPlus).use(VideoPlayer);
 app.mount('#app')
